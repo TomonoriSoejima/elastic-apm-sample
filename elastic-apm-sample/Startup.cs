5bdaa5c67c8d82984277d9a126f01bae855b3ad2
Tomonori Soejima <25199092+TomonoriSoejima@users.noreply.github.com>
2023-08-31 11:53:02 +0900

    adding a test to connect to db

--- elastic-apm-sample/Startup.cs
+++ elastic-apm-sample/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Data.SqlClient;
 
 namespace elastic_apm_sample
 {
@@ -55,8 +56,79 @@ namespace elastic_apm_sample
             
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+
+        private async Task SayHelloToAkito(HttpContext context)
+        {
+
+
+            var transaction = Elastic.Apm.Agent
+                     .Tracer.StartTransaction("MyTransaction", ApiConstants.TypeRequest);
+
+            try
+            {
+                //application code that is captured as a transaction
+                await context.Response.WriteAsync("Hello Akito!");
+                var just_wait = true;
+            }
+            catch (Exception e)
+            {
+                transaction.CaptureException(e);
+                throw;
+            }
+            finally
+            {
+                transaction.End();
+            }
+
+
+
+        }
+
+
+        private async Task SelectSomething(HttpContext context)
+        {
+
+
+
+
+            //"Persist Security Info=False;User ID=*****;Password=*****;Initial Catalog=AdventureWorks;Server=MySqlServer;Encrypt=True;"
+
+
+            string connectionString = "Server=localhost;Database=;User Id=sa;Password=Nzn6M_3M-X1s;";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string sqlQuery = "SELECT * FROM MySampleTable";
+
+                using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            int id = reader.GetInt32(0);
+                            string firstName = reader.GetString(1);
+                            string lastName = reader.GetString(2);
+                            int age = reader.GetInt32(3);
+                            string email = reader.GetString(4);
+
+                            Console.WriteLine($"ID: {id}, FirstName: {firstName}, LastName: {lastName}, Age: {age}, Email: {email}");
+                        }
+                    }
+                }
+            }
+        }
+            
+        
+
+
+
+
+    
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseAllElasticApm(Configuration);
             if (env.IsDevelopment())
@@ -74,6 +146,8 @@ namespace elastic_apm_sample
                 });
 
                 endpoints.MapGet("/tomo", SayHelloToTomo);
+                endpoints.MapGet("/akito", SayHelloToAkito);
+                endpoints.MapGet("/select", SelectSomething);
             });
         }
 
